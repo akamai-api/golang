86630c78e05ece8fbe359d49e1d9abd1b53d9e6d
Mohammadali Ghasemi <in.ghasemi@gmail.com>
2017-08-31 14:05:22 +0200

    Accessing to the properties are handled in a loop

--- Server/server.go
+++ Server/server.go
@@ -1,16 +1,80 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
+
+type AkamaiPayload struct {
+	CP     string            `json:"cp"`
+	Format string            `json:"format"`
+	Geo    map[string]string `json:"geo"`
+	// city
+	// country
+	// lat
+	// long
+	// region
+	ID      string            `json:"id"`
+	Message map[string]string `json:"message"`
+	// UA
+	// bytes
+	// cliIP
+	// fwdHost
+	// proto
+	// protoVer
+	// reqHost
+	// reqMethod
+	// reqPath
+	// reqPort
+	// respCT
+	// respLen
+	// status
+	NetPerf map[string]string `json:"netPerf"`
+	// asnum
+	// cacheStatus
+	// downloadTime
+	// edgeIP
+	// firstByte
+	// lastByte
+	// lastMileRTT
+	Network map[string]string `json:"network"`
+	// asnum
+	// edgeIP
+	// network
+	// networkType
+	ReqHdr map[string]string `json:"reqHdr"`
+	// cookie
+	RespHdr map[string]string `json:"respHdr"`
+	// server
+	// contEnc
+	Start   string `json:"start"`
+	Type    string `json:"type"`
+	Version string `json:"version"`
+}
+
+func CreateObject(jsonFile []byte) (_ []AkamaiPayload, err error) {
+
+	var arrayObject []AkamaiPayload
+	err = json.Unmarshal(jsonFile, &arrayObject)
+	return arrayObject, err
+}
 
 func Handle(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "OK! I got you Bro! -> ")
 	body, _ := ioutil.ReadAll(r.Body)
-	fmt.Println(string(body))
+	obj, err := CreateObject(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, o := range obj {
+		//payload[0].Geo["city"]
+		fmt.Println(o.Geo["city"])
+	}
 }
 
 func main() {
